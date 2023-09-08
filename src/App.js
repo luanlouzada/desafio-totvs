a6be41b63571226415ac4818a55eba8a8200c6ac
Luan <luanlouzadadev@gmail.com>
2023-09-08 17:48:51 -0300

    80% pronto

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  // Mantendo a senha inserida pelo usuário ou a gerada automaticamente
   const [password, setPassword] = useState('');
-
-  // Mantendo o número de visualizações que o usuário definiu
   const [views, setViews] = useState(0);
-
-  // Mantendo a duração em dias que o usuário definiu para a senha
   const [days, setDays] = useState(0);
+  const [showPopup, setShowPopup] = useState(false);
+  const [error, setError] = useState('');
 
-  // Minha URL base da API. Preciso lembrar de substituir pela URL real do API Gateway quando estiver pronto.
-  const API_URL = 'https://905rvzdjn9.execute-api.us-east-2.amazonaws.com/prod';
+  const API_URL = 'https://905rvzdjn9.execute-api.us-east-2.amazonaws.com/prod/password';
 
-  // Função para gerar uma senha aleatória. Estou usando SHA-256 para torná-la segura.
   const generatePassword = async () => {
     const randomString = Math.random().toString(36).substr(2, 8);
     const encoder = new TextEncoder();
@@ -23,11 +18,41 @@ function App() {
     const hash = await crypto.subtle.digest('SHA-256', data);
     const hashArray = Array.from(new Uint8Array(hash));
     const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-    setPassword(hashHex);
+
+    const specialChars = "!@#$%^&*()_+<>?";
+    const randomSpecialChar = specialChars[Math.floor(Math.random() * specialChars.length)];
+
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const randomUppercaseChar = uppercaseChars[Math.floor(Math.random() * uppercaseChars.length)];
+
+    setPassword(hashHex + randomSpecialChar + randomUppercaseChar);
   };
 
-  // Função para criar um link seguro. Aqui estou enviando os dados para minha API.
+  const isValidPassword = (pwd) => {
+    const hasUpperCase = /[A-Z]/.test(pwd);
+    const hasNumber = /[0-9]/.test(pwd);
+    const hasSpecialChar = /[!@#$%^&*()_+<>?]/.test(pwd);
+    return pwd.length >= 8 && hasUpperCase && hasNumber && hasSpecialChar;
+  };
+
   const createSecureLink = () => {
+    if (!isValidPassword(password)) {
+      setError("A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, um número e um caractere especial.");
+      return;
+    }
+
+    if (views < 1 || !Number.isInteger(Number(views))) {
+      setError("Número de visualizações deve ser um número inteiro maior ou igual a 1.");
+      return;
+    }
+
+    if (days < 1 || !Number.isInteger(Number(days))) {
+      setError("Validade (em dias) deve ser um número inteiro maior ou igual a 1.");
+      return;
+    }
+
+    setError('');
+
     const passwordData = {
       password,
       views,
@@ -36,8 +61,7 @@ function App() {
 
     axios.post(API_URL, passwordData)
       .then(response => {
-        // A API deve retornar a URL gerada ou alguma informação relevante. Vou manter isso aqui para referência futura.
-        console.log(response.data);
+        setShowPopup(true);
       })
       .catch(error => {
         console.error("Ops! Tive um problema ao tentar criar o link seguro:", error);
@@ -48,7 +72,6 @@ function App() {
     <div className="App">
       <h1>Gerador de Senha Segura</h1>
 
-      {/* Campo para o usuário inserir sua própria senha */}
       <input 
         type="text" 
         placeholder="Digite sua senha" 
@@ -56,12 +79,12 @@ function App() {
         onChange={(e) => setPassword(e.target.value)} 
       />
 
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
       <p>OU</p>
 
-      {/* Botão para o usuário gerar uma senha aleatória */}
       <button onClick={generatePassword}>Gerar Senha Aleatória</button>
 
-      {/* Campo para o usuário definir quantas vezes a senha pode ser visualizada */}
       <label>
         Número de visualizações:
         <input 
@@ -71,7 +94,6 @@ function App() {
         />
       </label>
 
-      {/* Campo para o usuário definir por quantos dias a senha será válida */}
       <label>
         Validade (em dias):
         <input 
@@ -81,9 +103,19 @@ function App() {
         />
       </label>
 
-      {/* Botão para enviar os dados para a API e obter o link seguro */}
       <button onClick={createSecureLink}>
         Criar Link Seguro
+      </button>
+
+      {showPopup && (
+        <div className="popup">
+          Sua senha foi criada com sucesso!
+          <button onClick={() => setShowPopup(false)}>Fechar</button>
+        </div>
+      )}
+
+      <button>
+        Recuperar Senha
       </button>
     </div>
   );
